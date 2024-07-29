8f1ec592ed9e8af2e6966fb80ee02eb2702fb053
A110210137 <A110210137@mail.shu.edu.tw>
2024-07-29 16:01:16 +0800

    113-07-29

--- 113-07-29/03_localiamge.py
+++ 113-07-29/03_localiamge.py
@@ -1,8 +1,8 @@
 import requests
 
 URL = 'https://notify-api.line.me/api/notify'
-H, P = {}, {}
+H, P, F = {}, {}, {}
 H['Aothorizaion'] = 'Bearer JMcCEGDAdAa6I3WqcK8e3Xj8Ev9QtzFeBr9hHtc6aAv'
-P['message'] = '今天是星期五晚上'
-requests.post(URL, headers=H, params=P)
-
+P['message'] = '圖片'
+F['imageFile'] = open('fsd.jpg','rb')
+requests.post(URL, headers=H, params=P, files=F)
